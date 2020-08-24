4ddeed3396a1d6e080b0cfcfb71ff67aa2c87438
aneesh2308 <58884933+aneesh2308@users.noreply.github.com>
2020-08-24 14:44:17 +0530

    Update CharacterDetail.js

--- React Project/src/components/CharacterDetail.js
+++ React Project/src/components/CharacterDetail.js
@@ -4,7 +4,7 @@ import { CharacterContext } from "../contexts/CharacterContexts";
 const CharacterDetails = ({ character }) => {
   const { dispatch } = useContext(CharacterContext);
   return (
-    <li onClick={() => dispatch({ type: "REMOVE_BOOK", id: character.id })}>
+    <li onClick={() => dispatch({ type: "REMOVE_CHARACTER", id: character.id })}>
       <div className="title">{character.title}</div>
       <div className="power">{character.power}</div>
     </li>
