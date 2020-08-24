7edc2bfd5d05102e11bbc9041fe3e6266498231b
aneesh2308 <58884933+aneesh2308@users.noreply.github.com>
2020-08-24 14:43:57 +0530

    Update CharacterReducer.js

--- React Project/src/reducer/CharacterReducer.js
+++ React Project/src/reducer/CharacterReducer.js
@@ -2,7 +2,7 @@ import uuid from "uuid/v1";
 
 export const characterReducer = (state, action) => {
   switch (action.type) {
-    case "ADD_BOOK":
+    case "ADD_CHARACTER":
       return [
         ...state,
         {
@@ -11,7 +11,7 @@ export const characterReducer = (state, action) => {
           id: uuid()
         }
       ];
-    case "REMOVE_BOOK":
+    case "REMOVE_CHARACTER":
       return state.filter((character) => character.id !== action.id);
     default:
       return state;
